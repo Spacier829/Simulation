959ef334f27deb1ba225083b34f03c01f05de06a
Spacier829 <mrsimpleeegor@gmail.com>
2024-10-16 14:26:31 +0700

    feat: implemented WorldMap class with methods, setters, getters

--- src/worldMap/WorldMap.java
+++ src/worldMap/WorldMap.java
@@ -3,15 +3,39 @@ package worldMap;
 import entities.Entity;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class WorldMap {
-  HashMap<Coordinates, Entity> entities = new HashMap<>();
+  private final int width;
+  private final int height;
+  private Map<Coordinates, Entity> entities = new HashMap<>();
+
+  public WorldMap(int width, int height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  public int getWidth() {
+    return width;
+  }
+
+  public int getHeight() {
+    return height;
+  }
 
   public void setEntity(Coordinates coordinates, Entity entity) {
     entities.put(coordinates, entity);
   }
 
-  public void setupDefaultEntityPosition(Entity entity) {
+  public void removeEntity(Coordinates coordinates) {
+    entities.remove(coordinates);
+  }
 
+  public Entity getEntity(Coordinates coordinates) {
+    return entities.get(coordinates);
+  }
+
+  public boolean isCellEmpty(Coordinates coordinates) {
+    return !entities.containsKey(coordinates);
   }
 }
