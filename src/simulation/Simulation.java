ea6e03e2e9298f7b7b241ab12e3230ad0f3475d6
Spacier829 <mrsimpleeegor@gmail.com>
2024-10-22 17:56:18 +0700

    feat: add method for simulate born of entities

--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -1,10 +1,18 @@
 package simulation;
 
 import worldMap.WorldMap;
+import worldMap.WorldMapRenderer;
 
 public class Simulation {
   private WorldMap map;
   private int stepsCounter;
+
+  public static void main(String[] args) {
+    WorldMapRenderer worldMapRenderer = new WorldMapRenderer();
+    WorldMap worldMap = new WorldMap(5, 5);
+    worldMapRenderer.mockSpawnMethod(worldMap);
+    worldMapRenderer.render(worldMap);
+  }
   // WorldMapRenderer
   // Actions - список действий, исполняемых перед стартом симуляции
 }
