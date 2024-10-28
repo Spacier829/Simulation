e6afb575f94b35257f05b72899e6a8ce9375cb42
Spacier829 <mrsimpleeegor@gmail.com>
2024-10-28 15:44:36 +0700

    refactor(main): changed values of speed (for herbivore and predator) and damage (for predator)

--- src/entities/creatures/Herbivore.java
+++ src/entities/creatures/Herbivore.java
@@ -2,7 +2,7 @@ package entities.creatures;
 
 public class Herbivore extends Creature {
   private final static int MAX_HEALTH = 5;
-  private final static int SPEED = 2;
+  private final static int SPEED = 1;
 
   public Herbivore() {
     super(SPEED, MAX_HEALTH);
