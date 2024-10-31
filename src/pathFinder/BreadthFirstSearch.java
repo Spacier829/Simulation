74542169cf00c4b67aa71a50109c94cfdda44c70
Spacier829 <mrsimpleeegor@gmail.com>
2024-10-31 13:44:31 +0700

    feat(findPath): initial BreadthFirstSearch class

---
+++ src/pathFinder/BreadthFirstSearch.java
@@ -0,0 +1,16 @@
+package pathFinder;
+
+import entities.creatures.Creature;
+import worldMap.Coordinates;
+import worldMap.WorldMap;
+
+import java.util.*;
+
+public class BreadthFirstSearch {
+  public List<Coordinates> findPath(WorldMap worldMap, Coordinates coordinates, Creature creature) {
+    Queue<Coordinates> queue = new LinkedList<>();
+    List<Coordinates> visitedCells = new ArrayList<>();
+    return visitedCells;
+    }
+  }
+}
