f86e42e5818af042b6d2b5ad7ac370060f2d1aa9
Spacier829 <mrsimpleeegor@gmail.com>
2024-11-01 17:27:00 +0700

    feat(findPath): implemented BFS class

--- src/pathFinder/BreadthFirstSearch.java
+++ src/pathFinder/BreadthFirstSearch.java
@@ -3,14 +3,25 @@ package pathFinder;
 import entities.creatures.Creature;
 import worldMap.Coordinates;
 import worldMap.WorldMap;
+import worldMap.WorldMapRenderer;
+import worldMap.WorldMapUtils;
 
 import java.util.*;
 
 public class BreadthFirstSearch {
+  private Queue<Coordinates> queue;
+  private List<Coordinates> visitedCells;
+  private Map<Coordinates, Coordinates> coordinatesConnections;
+
   public List<Coordinates> findPath(WorldMap worldMap, Coordinates coordinates, Creature creature) {
-    Queue<Coordinates> queue = new LinkedList<>();
+    queue = new LinkedList<>();
     List<Coordinates> visitedCells = new ArrayList<>();
+    coordinatesConnections = new HashMap<>();
+    WorldMapRenderer renderer = new WorldMapRenderer();
+
+    queue.add(coordinates);
+
     return visitedCells;
-    }
   }
+}
 }
