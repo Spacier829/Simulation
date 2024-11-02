7d83bd1dac5c18cac2591b2ec96a0aa6aa595400
Spacier829 <mrsimpleeegor@gmail.com>
2024-11-02 16:32:17 +0700

    test: debug of working

--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -11,12 +11,15 @@ import worldMap.Coordinates;
 import worldMap.WorldMap;
 import worldMap.WorldMapRenderer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Simulation {
   private WorldMap map;
   private int stepsCounter;
 
   public static void main(String[] args) {
-    WorldMap worldMap = new WorldMap(5,5);
+    WorldMap worldMap = new WorldMap(12, 12);
     WorldMapRenderer renderer = new WorldMapRenderer();
 
     Creature zebra = new Herbivore();
@@ -24,12 +27,24 @@ public class Simulation {
     Entity rock = new Rock();
     String test = Grass.class.getSimpleName();
 
-    worldMap.setEntity(new Coordinates(2,2), zebra);
-    worldMap.setEntity(new Coordinates(1,2), grass);
-    worldMap.setEntity(new Coordinates(1,3), rock);
+    worldMap.setEntity(new Coordinates(11, 11), zebra);
+    worldMap.setEntity(new Coordinates(1, 3), grass);
+    worldMap.setEntity(new Coordinates(1, 3), rock);
+
+    List<Coordinates> movableCreatures = new ArrayList<>();
+    for (Coordinates coordinates : worldMap.getEntitiesCoordinates()) {
+      Entity entity = worldMap.getEntity(coordinates);
+      if (entity instanceof Creature) {
+        movableCreatures.add(coordinates);
+      }
+    }
+
+    renderer.render(worldMap);
 
     BreadthFirstSearch bfs = new BreadthFirstSearch(worldMap);
-    bfs.findPath(worldMap, new Coordinates(2,2), zebra);
+    List<Coordinates> path = bfs.findPath(worldMap, movableCreatures.get(0), zebra);
+    // Проверки пути, что он не равен 0, если равен - пути нет
+    int a = 123;
   }
   // WorldMapRenderer
   // Actions - список действий, исполняемых перед стартом симуляции
